3944126a3bcac899c318c42b75278e4ce512042c
tehkum kanchwala <tehkumkanchwala@gmail.com>
2022-09-16 12:02:18 +0530

    files added

---
+++ script.js
@@ -0,0 +1,37 @@
+var billAmount = document.querySelector("#bill-amt");
+var cashAmount = document.querySelector("#cash-amt");
+var checkButton = document.querySelector("#click-bill");
+var message = document.querySelector("#error-mess");
+var noteNumber = document.querySelectorAll(".no-of-change");
+var numMoney= [2000,500,200,100,50,20,10,5,2,1];
+checkButton.addEventListener("click",function operationsCheck()
+{
+    message.style.display = "none";
+   
+    if(billAmount.value > 0)
+    {
+        if(billAmount.value >= cashAmount.value){
+            errorOperation("cash amount should be more");
+        }else{
+            var returnAmount = Number(cashAmount.value) - Number(billAmount.value);
+            totalAmountOfChange(returnAmount);
+            console.log(typeof returnAmount)
+        }
+    } else{
+        errorOperation("bill amount should be more than zero");
+    }
+
+});
+function errorOperation(msg){
+    message.style.display = "block";
+    message.innerText = msg;
+}
+function totalAmountOfChange(returnAmount){
+    for(let i = 0; i < numMoney.length; i++){
+        var distributionAmount = Math.trunc(returnAmount.value / numMoney[i]);
+        returnAmount = returnAmount%numMoney[i];
+        noteNumber[i].innerText=  distributionAmount; 
+        console.log(typeof returnAmount)
+    }
+    
+}
